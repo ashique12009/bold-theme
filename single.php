2b7ceb656ce9949a0057e9f15c379b39a1ee79c4
Ashique <ashique12009@gmail.com>
2021-07-24 19:34:01 +0600

    comments form added

--- single.php
+++ single.php
@@ -43,6 +43,12 @@
             </p>
             <?php the_content(); ?>
           </div>
+
+          <?php if (comments_open()) :?>
+          <div class="col-md-10 offset-md-1">
+            <?php comments_template();?>
+          </div>
+          <?php endif; ?>
         </div>
       </div>
     </div>
