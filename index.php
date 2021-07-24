f6447b760de4348a1312665b2abd83aa726217a0
Ashique <ashique12009@gmail.com>
2021-07-24 19:52:02 +0600

    hero template part added

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php get_header(); ?>
 
 <body <?php body_class();?>>
-  <div class="header">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <h3 class="tagline"><?php bloginfo('description'); ?></h3>
-          <h1 class="align-self-center display-1 text-center heading"><?php bloginfo('name'); ?></h1>
-        </div>
-      </div>
-    </div>
-  </div>
+  <?php get_template_part('hero-template-part'); ?>
   <div class="posts">
     <?php 
       while(have_posts()) {
